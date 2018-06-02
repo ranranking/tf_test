8964310e622cdd7d4864cbbb129bb3a342a0e832
Zhongqi.Miao <ranrankingking@gmail.com>
2018-06-01 17:50:47 -0700

    update

--- mnist_exe/model.py
+++ mnist_exe/model.py
@@ -12,7 +12,7 @@ def conv_layer(
     padding, 
     name):
 
-    with tf.variable.scope(name):
+    with tf.variable_scope(name):
 
         weights = tf.get_variable(
             name='weights', 
@@ -35,6 +35,11 @@ def conv_layer(
         relu = tf.nn.relu(
             features=bias)        
 
+        # Summaries
+        tf.summary.histogram("weights", weights)
+        tf.summary.histogram("biases", biases)
+        tf.summary.histogram("activations", relu)
+
         return relu
 
 def fc_layer(
@@ -44,7 +49,7 @@ def fc_layer(
     name,
     is_relu):
     
-    with tf.variable.scope(name):
+    with tf.variable_scope(name):
 
         weights = tf.get_variable(
             name='weights', 
@@ -54,14 +59,23 @@ def fc_layer(
             name='biases', 
             shape=[num_output])
 
-        bias = tf.nn.wx_plus_b(
+        bias = tf.nn.xw_plus_b(
             x=x,
             weights=weights, 
             biases=biases)
 
+        # Summaries
+        tf.summary.histogram("weights", weights)
+        tf.summary.histogram("biases", biases)
+
         if is_relu:
-            return tf.nn.relu(
+
+            relu = tf.nn.relu(
                 features=bias)
+
+            tf.summary.histogram("relu", relu)
+
+            return relu
         else:
             return bias
 
@@ -83,20 +97,16 @@ def max_pool(
 
 def dropout(
     x,
-    keep_rate,
-    is_train):
+    keep_rate):
     
-    return tf.cond(
-        pred=is_train,
-        true_fn=lambda: tf.dropout(
-            x=x,
-            keep_prob=keep_rate),
-        false_fn=lambda: x)
-
-def model(
+    return tf.nn.dropout(
+        x=x,
+        keep_prob=keep_rate)
+    
+def build_model(
     x,
     labels,
-    train_mode):
+    keep_rate):
 
     # Conv layer #1
     conv1 = conv_layer(
@@ -152,28 +162,21 @@ def model(
         num_input=7 * 7 * 64,
         num_output=1024, 
         name='fc5',
-        is_relu=True):
+        is_relu=True)
 
     drop = dropout(
         x=fc5,
-        keep_rate=0.4,
-        is_train=train_mode == 'TRAIN'):
+        keep_rate=keep_rate)
 
     # Logit layer #6
-    fc6 = fc_layer(
+    logits = fc_layer(
         x=drop,
         num_input=1024,
         num_output=10, 
         name='fc6',
-        is_relu=False):
+        is_relu=False)
 
-    predictions = {
-        'classes': tf.argmax(
-            input=fc6,
-            axis=1),
-        'probabilities': tf.nn.softmax(
-            logits=fc6, 
-            name='prob')
-        }
+    return logits, fc5
+
 
          
